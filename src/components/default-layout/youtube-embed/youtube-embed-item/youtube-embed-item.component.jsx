77cc8220cdfe324b7ac3fa2f39837f040c277313
Atebisun David <davidolaoluwa240@gmail.com>
2023-12-23 05:17:31 +0100

    Made changes to the youtube emebed item component

--- src/components/default-layout/youtube-embed/youtube-embed-item/youtube-embed-item.component.jsx
+++ src/components/default-layout/youtube-embed/youtube-embed-item/youtube-embed-item.component.jsx
@@ -6,8 +6,9 @@ export const YoutubeEmbedItem = ({ src, title }) => {
       src={src}
       title={title}
       frameBorder="0"
-      allow="accelerometer; encrypted-media"
-      allowFullScreen
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
     />
   );
 };
--- src/components/home/membership-professional-body-base/membership-professional-body-item/membership-professional-body-item.component.jsx
+++ src/components/home/membership-professional-body-base/membership-professional-body-item/membership-professional-body-item.component.jsx
@@ -1,7 +1,7 @@
 export const MembershipProfessionalBodyItem = ({ title }) => {
   return (
     <li
-      className="text-lg text-black text-justify"
+      className="text-lg text-black text-justify break-words"
       dangerouslySetInnerHTML={{ __html: title }}
     />
   );
